ff009c6024f5581a2239d872e84a0c19da8968f9
yelosolutions <yelosolutions69@gmail.com>
2022-03-28 19:19:19 +0000

    *_strspn - gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- *  * _strchr - locates a character in a string,
- *   * @s: string.
- *    * @c: character.
- *     * Return: the pointer to the first occurrence of the character c.
- *      */
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
