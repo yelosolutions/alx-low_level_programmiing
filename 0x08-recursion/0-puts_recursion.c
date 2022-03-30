5c40de9e87f6e94351a5360ccccb6a8ba599400c
yelosolutions <yelosolutions69@gmail.com>
2022-03-30 13:02:43 +0000

    _puts_recursion - prints a string followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,11 +2,11 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s - string to print
+ * @s: string to print
  *
  */
 void _puts_recursion(char *s)
-{	
+{
 	if (*s == '\0')
 	{
 		_putchar('\n');
