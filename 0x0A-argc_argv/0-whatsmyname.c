8e84bf56b9149a51404519496a11ed5a34255399
yelosolutions <yelosolutions69@gmail.com>
2022-04-01 09:49:40 +0000

    Program prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the file name
+ * @argc: the number of commang line arguments or size of argv[]
+ * @argv: array of size argc or array containing the program command line arguments
+ *
+ * Return: always 0. 
+ */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	_putchar(argv[0]);
+
+	return (0);
+}
