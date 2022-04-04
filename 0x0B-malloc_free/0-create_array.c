84b049c0527ca023281c8908b2ee1b1b01fbf938
yelosolutions <yelosolutions69@gmail.com>
2022-04-04 15:47:04 +0000

    *create_array - creates an array of chars, initiated by a specific character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars.
+ * @c: characters to print
+ * @size: size of memory
+ *
+ * Return: NULL if size = 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = (char *) malloc(sizeof(char) * size);
+
+	if(ptr == NULL)
+		return (0);
+
+	while (i < size)
+	{
+		*(ptr + i) = c;
+		i++;
+	}
+
+	*(ptr + i) = '\0';
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+I intend to keep my malloc and free tasks files in this directory.
+ 
