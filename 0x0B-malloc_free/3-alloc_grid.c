99245a54cb40c4f5c5793030857f3511559021fa
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-04-05 22:38:15 +0300

    **alloc_grid - creates a two dimensional array of ints

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,35 +11,35 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **arr;
-    int i, j;
+int **arr;
+int i, j;
 
-    if (height <= 0 || width <= 0)
-        return (NULL);
+if (height <= 0 || width <= 0)
+return (NULL);
 
-    arr = (int **)malloc(sizeof(int *) * height);
+arr = (int **)malloc(sizeof(int *) * height);
 
-    if (arr == NULL)
-        return (NULL);
+if (arr == NULL)
+return (NULL);
 
-    for (i = 0; i < height; i++)
-    {
-        arr[i] = (int *)malloc(sizeof(int) * width);
-        if (arr[i] == NULL)
-        {
-            free(arr);
-            for (j = 0; j <= i; j++)
-                free(arr[j]);
-            return (NULL);
-        }
-    }
+for (i = 0; i < height; i++)
+{
+arr[i] = (int *)malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+free(arr);
+for (j = 0; j <= i; j++)
+free(arr[j]);
+return (NULL);
+}
+}
 
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
-    return (arr);
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
 }
