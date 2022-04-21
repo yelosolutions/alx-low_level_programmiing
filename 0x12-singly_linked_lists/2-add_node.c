7a3e777a40e8efc2b7e3f820af6994a897da71b9
yelosolutions <yelosolutions69@gmail.com>
2022-04-21 20:04:06 +0000

    add_node - adds node at the beginning of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,23 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
- * @head: pointer to the pointer to the first node of the list.
- * @str: string to be added to the node.
- * Return: address of the new element, or NULL if it failed.
- */
+ * add_node - adds a new node at the beginning of a linked list 
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ *  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t nchar;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
-	for (nchar = 0; str[nchar] != '\0'; nchar++)
-		;
-	new_node->len = nchar;
-	new_node->next = *head;
-	*head = new_node;
-	return (*head);
+	list_t *temp;
+
+	if (head != NULL && str != NULL)
+	{
+		temp = malloc(sizeof(list_t))
+			if (temp == NULL)
+				return (NULL);
+
+		temp->str = strdup(str);
+		temp->len = _strlen(str);
+		temp->next = *head;
+
+		*head = temp;
+
+		return (temp);
+	}
+
+	return (0);
+}
+
+/**
+ * _strlen - Returns the lenth of a string
+ * @s: string to count
+ *  
+ *  Return: string lenth
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
 }
