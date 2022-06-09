da4d43947bad8288a94d0774c9286d45c60d0048
yelosolutions <yelosolutions69@gmail.com>
2022-06-09 22:07:42 +0000

    frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *  * free_dlistint - frees a dlistint_t list
+ *   *
+ *    * @head: first node of the list
+ *     * Return: no return
+ *      */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
