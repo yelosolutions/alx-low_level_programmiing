27efa12c6b863b95aa91262a5426d2eeb576e685
yelosolutions <yelosolutions69@gmail.com>
2022-06-09 22:32:47 +0000

    deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@
  * @head: first node of the list
  * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
- *  */
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h1;
