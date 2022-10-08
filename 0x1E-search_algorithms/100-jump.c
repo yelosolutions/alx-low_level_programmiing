42155268bb54ee2a84af0fdb09f834f3ffeb31c4
yelosolutions <yelosolutions69@gmail.com>
2022-10-08 13:50:39 +0000

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,45 +12,45 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t start, end, step;
+	size_t start, end, step;
 
-    if (!array || size == 0)
-        return (-1);
-    step = sqrt(size);
-    start = 0, end = step;
+	if (!array || size == 0)
+		return (-1);
+	step = sqrt(size);
+	start = 0, end = step;
 
-    while (start < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-        if (end < size)
-        {
-            if (array[start] <= value && value <= array[end])
-            {
-                printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-                break;
-            }
-        }
-        else
-        {
-            if (array[start] <= value)
-            {
-                printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-                break;
-            }
-        }
-        start = end;
-        end = start + step;
-    }
+	while (start < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		if (end < size)
+		{
+			if (array[start] <= value && value <= array[end])
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+				break;
+			}
+		}
+		else
+		{
+			if (array[start] <= value)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+				break;
+			}
+		}
+		start = end;
+		end = start + step;
+	}
 
-    while (start <= end)
-    {
-        if (start == size)
-            return (-1);
-        printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-        if (array[start] == value)
-            return (start);
-        start++;
-    }
+	while (start <= end)
+	{
+		if (start == size)
+			return (-1);
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+	}
 
-    return (-1);
+	return (-1);
 }
